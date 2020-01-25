281167b41a050682b8df2e338927b30e01d90687
Paul Osetinsky <p.osetinsky@gmail.com>
2020-01-25 14:53:42 -0500

    new go func

--- gstreamer-send/jack/jack.go
+++ gstreamer-send/jack/jack.go
@@ -12,79 +12,83 @@ import (
 )
 
 func StartGstreamer(browserToken string, ch chan string) {
-  audioSrc := flag.String("audio-src", "jackaudiosrc ! audioconvert ! audioresample", "GStreamer audio src")
-  flag.Parse()
 
-  // Everything below is the pion-WebRTC API! Thanks for using it ❤️.
+  go func() {
+    audioSrc := flag.String("audio-src", "jackaudiosrc ! audioconvert ! audioresample", "GStreamer audio src")
+    flag.Parse()
 
-  // Prepare the configuration
-  config := webrtc.Configuration{
-    ICEServers: []webrtc.ICEServer{
-      {
-        URLs: []string{"stun:stun.l.google.com:19302"},
+    // Everything below is the pion-WebRTC API! Thanks for using it ❤️.
+
+    // Prepare the configuration
+    config := webrtc.Configuration{
+      ICEServers: []webrtc.ICEServer{
+        {
+          URLs: []string{"stun:stun.l.google.com:19302"},
+        },
       },
-    },
-  }
+    }
 
-  // Create a new RTCPeerConnection
-  peerConnection, err := webrtc.NewPeerConnection(config)
-  if err != nil {
-    panic(err)
-  }
+    // Create a new RTCPeerConnection
+    peerConnection, err := webrtc.NewPeerConnection(config)
+    if err != nil {
+      panic(err)
+    }
 
-  // Set the handler for ICE connection state
-  // This will notify you when the peer has connected/disconnected
-  peerConnection.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
-    fmt.Printf("Connection State has changed %s \n", connectionState.String())
-  })
+    // Set the handler for ICE connection state
+    // This will notify you when the peer has connected/disconnected
+    peerConnection.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
+      fmt.Printf("Connection State has changed %s \n", connectionState.String())
+    })
 
-  // Create a audio track
-  audioTrack, err := peerConnection.NewTrack(webrtc.DefaultPayloadTypeOpus, rand.Uint32(), "audio", "pion1")
-  if err != nil {
-    panic(err)
-  }
-  _, err = peerConnection.AddTrack(audioTrack)
-  if err != nil {
-    panic(err)
-  }
+    // Create a audio track
+    audioTrack, err := peerConnection.NewTrack(webrtc.DefaultPayloadTypeOpus, rand.Uint32(), "audio", "pion1")
+    if err != nil {
+      panic(err)
+    }
+    _, err = peerConnection.AddTrack(audioTrack)
+    if err != nil {
+      panic(err)
+    }
 
-  // Wait for the offer to be pasted
-  // offer := webrtc.SessionDescription{}
-  // signal.Decode(signal.MustReadStdin(), &offer)
+    // Wait for the offer to be pasted
+    // offer := webrtc.SessionDescription{}
+    // signal.Decode(signal.MustReadStdin(), &offer)
 
-  offer := webrtc.SessionDescription{}
-  // signal.Decode(signal.MustReadStdin(), &offer)
-  signal.Decode(browserToken, &offer)
+    offer := webrtc.SessionDescription{}
+    // signal.Decode(signal.MustReadStdin(), &offer)
+    signal.Decode(browserToken, &offer)
 
-  // Set the remote SessionDescription
-  err = peerConnection.SetRemoteDescription(offer)
-  if err != nil {
-    panic(err)
-  }
+    // Set the remote SessionDescription
+    err = peerConnection.SetRemoteDescription(offer)
+    if err != nil {
+      panic(err)
+    }
 
-  // Create an answer
-  answer, err := peerConnection.CreateAnswer(nil)
-  if err != nil {
-    panic(err)
-  }
+    // Create an answer
+    answer, err := peerConnection.CreateAnswer(nil)
+    if err != nil {
+      panic(err)
+    }
 
-  // Sets the LocalDescription, and starts our UDP listeners
-  err = peerConnection.SetLocalDescription(answer)
-  if err != nil {
-    panic(err)
-  }
+    // Sets the LocalDescription, and starts our UDP listeners
+    err = peerConnection.SetLocalDescription(answer)
+    if err != nil {
+      panic(err)
+    }
 
-  ch <-signal.Encode(answer)
-  // close(ch)
+    ch <-signal.Encode(answer)
+    // close(ch)
 
-  // Output the answer in base64 so we can paste it in browser
-  fmt.Println(signal.Encode(answer))
+    // Output the answer in base64 so we can paste it in browser
+    fmt.Println(signal.Encode(answer))
 
-  // Start pushing buffers on these tracks
-  gst.CreatePipeline(webrtc.Opus, []*webrtc.Track{audioTrack}, *audioSrc).Start()
+    // Start pushing buffers on these tracks
+    gst.CreatePipeline(webrtc.Opus, []*webrtc.Track{audioTrack}, *audioSrc).Start()
 
-  // Block forever
-  select {}
+    // Block forever
+    select {}
+
+  }()
 }
 
 
