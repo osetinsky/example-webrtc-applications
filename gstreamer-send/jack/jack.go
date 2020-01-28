87b66ff7c061dcbfaf00d1bff1f1de924a315003
Paul Osetinsky <p.osetinsky@gmail.com>
2020-01-28 01:26:18 -0500

    use local stun server

--- gstreamer-send/jack/jack.go
+++ gstreamer-send/jack/jack.go
@@ -23,7 +23,8 @@ func StartGstreamer(flagName, browserToken string, ch chan string) {
     config := webrtc.Configuration{
       ICEServers: []webrtc.ICEServer{
         {
-          URLs: []string{"stun:stun.l.google.com:19302"},
+          URLs: []string{"stun:0.0.0.0:3478"},
+          // URLs: []string{"stun:stun.l.google.com:19302"},
         },
       },
     }
