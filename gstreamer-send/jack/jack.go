f054d3475a085258cb5466d1a04b38619e6e310f
Paul Osetinsky <p.osetinsky@gmail.com>
2020-01-28 01:35:13 -0500

    stun test

--- gstreamer-send/jack/jack.go
+++ gstreamer-send/jack/jack.go
@@ -23,7 +23,7 @@ func StartGstreamer(flagName, browserToken string, ch chan string) {
     config := webrtc.Configuration{
       ICEServers: []webrtc.ICEServer{
         {
-          URLs: []string{"0.0.0.0:3478"},
+          URLs: []string{"stun:0.0.0.0:3478"},
           // URLs: []string{"stun:stun.l.google.com:19302"},
         },
       },
