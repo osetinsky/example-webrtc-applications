0693d8898e52f6899405347894c67fd4aeb3a8f4
Paul Osetinsky <p.osetinsky@gmail.com>
2020-02-05 22:12:57 -0500

    chan to close

--- gstreamer-send/jack/jack.go
+++ gstreamer-send/jack/jack.go
@@ -12,85 +12,87 @@ import (
 )
 
 func StartGstreamer(flagName, browserToken string, ch chan string) {
+  audioSrc := flag.String(flagName, "jackaudiosrc ! audioconvert ! audioresample", "GStreamer audio src")
+  flag.Parse()
 
-  // go func() {
-    audioSrc := flag.String(flagName, "jackaudiosrc ! audioconvert ! audioresample", "GStreamer audio src")
-    flag.Parse()
+  // Everything below is the pion-WebRTC API! Thanks for using it ❤️.
 
-    // Everything below is the pion-WebRTC API! Thanks for using it ❤️.
-
-    // Prepare the configuration
-    config := webrtc.Configuration{
-      ICEServers: []webrtc.ICEServer{
-        {
-          URLs: []string{"stun:stun.l.google.com:19302"},
-        },
+  // Prepare the configuration
+  config := webrtc.Configuration{
+    ICEServers: []webrtc.ICEServer{
+      {
+        URLs: []string{"stun:stun.l.google.com:19302"},
       },
+    },
+  }
+
+  // Create a new RTCPeerConnection
+  peerConnection, err := webrtc.NewPeerConnection(config)
+  if err != nil {
+    panic(err)
+  }
+
+  // Set the handler for ICE connection state
+  // This will notify you when the peer has connected/disconnected
+
+  shouldTerminate := make(chan bool, false)
+
+  peerConnection.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
+    fmt.Printf("Connection State has changed %s \n", connectionState.String())
+    if connectionState.String() == "disconnected" {
+      fmt.Printf("Connection State has disconnected. Terminating. \n")
+      shouldTerminate<-true
     }
+  })
 
-    // Create a new RTCPeerConnection
-    peerConnection, err := webrtc.NewPeerConnection(config)
-    if err != nil {
-      panic(err)
-    }
+  // Create a audio track
+  audioTrack, err := peerConnection.NewTrack(webrtc.DefaultPayloadTypeOpus, rand.Uint32(), "audio", "pion1")
+  if err != nil {
+    panic(err)
+  }
+  _, err = peerConnection.AddTrack(audioTrack)
+  if err != nil {
+    panic(err)
+  }
 
-    // Set the handler for ICE connection state
-    // This will notify you when the peer has connected/disconnected
-    peerConnection.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
-      fmt.Printf("Connection State has changed %s \n", connectionState.String())
-      if connectionState.String() == "disconnected" {
-        fmt.Printf("Connection State has disconnected. Returning. \n")
-        return
-      }
-    })
+  // Wait for the offer to be pasted
+  // offer := webrtc.SessionDescription{}
+  // signal.Decode(signal.MustReadStdin(), &offer)
 
-    // Create a audio track
-    audioTrack, err := peerConnection.NewTrack(webrtc.DefaultPayloadTypeOpus, rand.Uint32(), "audio", "pion1")
-    if err != nil {
-      panic(err)
-    }
-    _, err = peerConnection.AddTrack(audioTrack)
-    if err != nil {
-      panic(err)
-    }
+  offer := webrtc.SessionDescription{}
+  // signal.Decode(signal.MustReadStdin(), &offer)
+  signal.Decode(browserToken, &offer)
 
-    // Wait for the offer to be pasted
-    // offer := webrtc.SessionDescription{}
-    // signal.Decode(signal.MustReadStdin(), &offer)
+  // Set the remote SessionDescription
+  err = peerConnection.SetRemoteDescription(offer)
+  if err != nil {
+    panic(err)
+  }
 
-    offer := webrtc.SessionDescription{}
-    // signal.Decode(signal.MustReadStdin(), &offer)
-    signal.Decode(browserToken, &offer)
+  // Create an answer
+  answer, err := peerConnection.CreateAnswer(nil)
+  if err != nil {
+    panic(err)
+  }
 
-    // Set the remote SessionDescription
-    err = peerConnection.SetRemoteDescription(offer)
-    if err != nil {
-      panic(err)
-    }
+  // Sets the LocalDescription, and starts our UDP listeners
+  err = peerConnection.SetLocalDescription(answer)
+  if err != nil {
+    panic(err)
+  }
 
-    // Create an answer
-    answer, err := peerConnection.CreateAnswer(nil)
-    if err != nil {
-      panic(err)
-    }
+  ch <-signal.Encode(answer)
 
-    // Sets the LocalDescription, and starts our UDP listeners
-    err = peerConnection.SetLocalDescription(answer)
-    if err != nil {
-      panic(err)
-    }
+  // Output the answer in base64 so we can paste it in browser
+  fmt.Println(signal.Encode(answer))
 
-    ch <-signal.Encode(answer)
-    // close(ch)
+  // Start pushing buffers on these tracks
+  gst.CreatePipeline(webrtc.Opus, []*webrtc.Track{audioTrack}, *audioSrc).Start()
 
-    // Output the answer in base64 so we can paste it in browser
-    fmt.Println(signal.Encode(answer))
-
-    // Start pushing buffers on these tracks
-    gst.CreatePipeline(webrtc.Opus, []*webrtc.Track{audioTrack}, *audioSrc).Start()
-
-    // Block forever
-    select {}
-
-  // }()
+  // Block forever unless shouldTerminate channel sends true
+  select {
+  case <-shouldTerminate:
+    break
+  default:
+  }
 }
